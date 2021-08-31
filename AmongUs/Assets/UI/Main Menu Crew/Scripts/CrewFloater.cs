54b6fa69db02f38cb306c56c29a68d3f5d73432a
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2021-09-01 00:49:44 +0900

    메인메뉴 UI - 3
    - 메인메뉴 배경에 플레이어 떠다니게하기

---
+++ AmongUs/Assets/UI/Main Menu Crew/Scripts/CrewFloater.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrewFloater : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab; // 소환해서 날려보낼 
+    [SerializeField] private List<Sprite> sprites;  // 크루원 이미지를 바꿔줄 리스트
+
+    private bool[] crewStates = new bool[12];    // 떠다니는 크루원의 색 중복을 방지할
+    private float timer = 0.5f;    // 크루원을 소환할 간격
+    private float distance = 11f;    // 중심으로부터 소환될 위치를 지정
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<12; i++)
+        {
+            SpawnFloatingCrew((EPlayerColor)i, Random.Range(0f, distance));
+            timer = 1f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0f)
+        {
+            SpawnFloatingCrew((EPlayerColor)Random.Range(0, 12), distance);
+            timer = 1f;
+        }
+    }
+
+    public void SpawnFloatingCrew(EPlayerColor playerColor, float dist) // 크루원을 소환
+    {
+        if (!crewStates[(int)playerColor])
+        {
+            crewStates[(int)playerColor] = true;
+
+            float angle = Random.Range(0f, 360f);
+            Vector3 spawnPos = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0f) * dist;
+            Vector3 direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+            float floatingSpeed = Random.Range(1f, 4f);
+            float rotateSpeed = Random.Range(-3f, 3f);
+
+            var crew = Instantiate(prefab, spawnPos, Quaternion.identity).GetComponent<FloatigCrew>();
+            crew.SetFloatingCrew(sprites[Random.Range(0, sprites.Count)], playerColor, direction, floatingSpeed, rotateSpeed, Random.Range(0.5f, 1f));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var crew = collision.GetComponent<FloatigCrew>();
+        if (crew != null)
+        {
+            crewStates[(int)crew.playerColor] = false;
+            Destroy(crew.gameObject);
+        }
+    }
+}
---
+++ AmongUs/Assets/UI/Main Menu Crew/Scripts/FloatigCrew.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatigCrew : MonoBehaviour
+{
+    public EPlayerColor playerColor;
+
+    private SpriteRenderer spriteRenderer;  //떠다니는 크루원의 색상
+    private Vector3 direction;  //크루원이 날아다니는 방향
+    private float floatingSpeed;    //크루원이 날아다니는 속도
+    private float rotateSpeed;   // 날아다니면서 회전하는 속도
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetFloatingCrew(Sprite sprite, EPlayerColor playerColor, Vector3 direction, float floatingSpeed, float rotateSpeed, float size)
+    {
+        this.playerColor = playerColor;
+        this.direction = direction;
+        this.floatingSpeed = floatingSpeed;
+        this.rotateSpeed = rotateSpeed;
+
+        spriteRenderer.sprite = sprite;
+        spriteRenderer.material.SetColor("_PlayerColor", PlayerColor.GetColor(playerColor));
+
+        transform.localScale = new Vector3(size, size, size);
+        spriteRenderer.sortingOrder = (int)Mathf.Lerp(1, 32767, size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * floatingSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, 0f, rotateSpeed));
+    }
+}
