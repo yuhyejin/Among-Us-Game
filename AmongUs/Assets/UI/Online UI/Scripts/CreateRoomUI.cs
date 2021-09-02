4f24a0180906918fe394f38bd31e84ea66429441
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2021-09-02 23:39:58 +0900

    온라인 UI와 방 만들기 UI
    - 온라인 -> 방 만들기 UI
    - 인원 수, 최대인원 등 기능 설정

---
+++ AmongUs/Assets/UI/Online UI/Scripts/CreateRoomUI.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateRoomUI : MonoBehaviour
+{
+    [SerializeField] private List<Image> crewImgs;  // 크루원 이미지를 담는 변수
+    [SerializeField] private List<Button> imposterCountButtons;
+    [SerializeField] private List<Button> maxPlayerCountButtons;
+
+    private CreateGameRoomData roomData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<crewImgs.Count; i++)
+        {
+            Material materialInstance = Instantiate(crewImgs[i].material);
+            crewImgs[i].material = materialInstance;
+        }
+
+        roomData = new CreateGameRoomData() { imposterCount = 1, maxPlayerCount = 10 };
+        UpdateCrewImages();
+    }
+
+    public void UpdateMaxPlayerCount(int count)     // 인원수에 맞게 버튼을 보여주는 함수
+    {
+        roomData.maxPlayerCount = count;
+
+        for (int i = 0; i < maxPlayerCountButtons.Count; i++)
+        {
+            if(i == count - 4)
+            {
+                maxPlayerCountButtons[i].image.color = new Color(1f, 1f, 1f, 1f);
+            }
+            else
+            {
+                maxPlayerCountButtons[i].image.color = new Color(1f, 1f, 1f, 0f);
+            }
+        }
+
+        UpdateCrewImages();
+    }
+
+    private void UpdateCrewImages()
+    {
+        for (int i=0; i < crewImgs.Count; i++)
+        {
+            crewImgs[i].material.SetColor("_PlayerColor", Color.white);
+        }
+
+        int imposterCount = roomData.imposterCount;
+        int idx = 0;
+        while(imposterCount != 0)
+        {
+            if(idx >= roomData.maxPlayerCount)
+            {
+                idx = 0;
+            }
+
+            if(crewImgs[idx].material.GetColor("_PlayerColor") != Color.red && Random.Range(0,5) == 0)
+            {
+                crewImgs[idx].material.SetColor("_PlayerColor", Color.red);
+                imposterCount--;
+            }
+            idx++;
+        }
+
+        for (int i=0; i<crewImgs.Count; i++)
+        {
+            if (i < roomData.maxPlayerCount)
+            {
+                crewImgs[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                crewImgs[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
+
+public class CreateGameRoomData
+{
+    public int imposterCount;
+    public int maxPlayerCount;
+}
