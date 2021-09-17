817c90f8f6ff4b5311baec1fa53075393c7e9dd0
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2021-09-17 16:59:53 +0900

    게임 규칙 설정 기능 구현
    - 게임 규칙 UI 기능 구현
    - 서브 패널 교체 기능 구현

--- AmongUs/Assets/Scripts/GameRuleStore.cs
+++ AmongUs/Assets/Scripts/GameRuleStore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Mirror;
+using System.Text;
 
 public enum EKillRange
 {
@@ -36,87 +37,330 @@ public struct GameRuleData
 
 public class GameRuleStore : NetworkBehaviour
 {
-    [SyncVar]
+    [SyncVar(hook = nameof(SetIsRecommendRule_Hook))]
     private bool isRecommendRule;
     [SerializeField]
     private Toggle isRecommendRuleToggle;
-    [SyncVar]
+    public void SetIsRecommendRule_Hook(bool _, bool value)
+    {
+        UpdateGameRuleOverview();
+    }
+    public void OnRecommendToggle(bool value)
+    {
+        isRecommendRule = value;
+        if (isRecommendRule)
+        {
+            SetRecommendGameRule();
+        }
+    }
+
+    [SyncVar(hook = nameof(SetConfirmEjects_Hook))]
     private bool confirmEjects;
     [SerializeField]
     private Toggle confirmEjectsToggle;
-    [SyncVar]
+    public void SetConfirmEjects_Hook(bool _, bool value)
+    {
+        UpdateGameRuleOverview();
+    }
+    public void OnConfirmEjectsToggle(bool value)
+    {
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+        confirmEjects = value;
+    }
+
+    [SyncVar (hook = nameof(SetEmergencyMeetings_Hook))]
     private int emergencyMeetings;
     [SerializeField]
     private Text emergencyMeetingsText;
-    [SyncVar]
+    public void SetEmergencyMeetings_Hook(int _, int value)
+    {
+        emergencyMeetingsText.text = value.ToString();
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeEmergencyMeetings(bool isPlus)
+    {
+        emergencyMeetings  = Mathf.Clamp(emergencyMeetings + (isPlus ? 1 : -1), 0, 9);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar(hook = nameof(SetEmergencyMeetingsCooldown_Hook))]
     private int emergencyMeetingsCooldown;
     [SerializeField]
     private Text emergencyMeetingsCooldownText;
-    [SyncVar]
+    public void SetEmergencyMeetingsCooldown_Hook(int _, int value)
+    {
+        emergencyMeetingsCooldownText.text = string.Format("{0}s", value);
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeEmergencyMeetingsCooldown(bool isPlus)
+    {
+        emergencyMeetingsCooldown = Mathf.Clamp(emergencyMeetingsCooldown + (isPlus ? 5 : -5), 0, 60);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar (hook = nameof(SetMeetingsTime_Hook))]
     private int meetingsTime;
     [SerializeField]
     private Text meetingsTimeText;
-    [SyncVar]
+    public void SetMeetingsTime_Hook(int _, int value)
+    {
+        meetingsTimeText.text = string.Format("{0}s", value);
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeMeetingsTime(bool isPlus)
+    {
+        meetingsTime = Mathf.Clamp(meetingsTime + (isPlus ? 5 : -5), 0, 120);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar (hook = nameof(SetVoteTime_Hook))]
     private int voteTime;
     [SerializeField]
     private Text voteTimeText;
-    [SyncVar]
+    public void SetVoteTime_Hook(int _, int value)
+    {
+        voteTimeText.text = string.Format("{0}s", value);
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeVoteTime(bool isPlus)
+    {
+        voteTime = Mathf.Clamp(voteTime + (isPlus ? 5 : -5), 0, 300);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar (hook = nameof(SetAnonymousVotes_Hook))]
     private bool anonymousVotes;
     [SerializeField]
     private Toggle anonymousVotesToggle;
-    [SyncVar]
+    public void SetAnonymousVotes_Hook(bool _, bool value)
+    {
+        UpdateGameRuleOverview();
+    }
+    public void OnAnonymousVotesToggle(bool value)
+    {
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+        anonymousVotes = value;
+    }
+
+    [SyncVar (hook = nameof(SetMeetingsTime_Hook))]
     private float moveSpeed;
     [SerializeField]
     private Text moveSpeedText;
-    [SyncVar]
+    public void SetMoveSpeed_Hook(float _, float value)
+    {
+        moveSpeedText.text = string.Format("{0:0.0}x", value);
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeMoveSpeed(bool isPlus)
+    {
+        moveSpeed = Mathf.Clamp(moveSpeed + (isPlus ? 0.25f : -0.25f), 0.5f, 3f);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar (hook = nameof(SetCrewSight_Hook))]
     private float crewSight;
     [SerializeField]
     private Text crewSightText;
-    [SyncVar]
+    public void SetCrewSight_Hook(float _, float value)
+    {
+        crewSightText.text = string.Format("{0:0.0}x", value);
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeCrewSight(bool isPlus)
+    {
+        crewSight = Mathf.Clamp(crewSight + (isPlus ? 0.25f : -0.25f), 0.25f, 5f);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar (hook = nameof(SetImposterSight_Hook))]
     private float imposterSight;
     [SerializeField]
     private Text imposterSightText;
-    [SyncVar]
+    public void SetImposterSight_Hook(float _, float value)
+    {
+        imposterSightText.text = string.Format("{0:0.0}x", value);
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeImposterSight(bool isPlus)
+    {
+        imposterSight = Mathf.Clamp(imposterSight + (isPlus ? 0.25f : -0.25f), 0.25f, 5f);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar(hook = nameof(SetKillCooldown_Hook))]
     private float killCooldown;
     [SerializeField]
     private Text killCooldownText;
-    [SyncVar]
+    public void SetKillCooldown_Hook(float _, float value)
+    {
+        killCooldownText.text = string.Format("{0:0.0}s", value);
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeKillCooldown(bool isPlus)
+    {
+        killCooldown = Mathf.Clamp(killCooldown + (isPlus ? 2.5f : -2.5f), 10f, 60f);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar(hook = nameof(SetKillRange_Hook))]
     private EKillRange killRange;
     [SerializeField]
     private Text killRangeText;
-    [SyncVar]
+    public void SetKillRange_Hook(EKillRange _, EKillRange value)
+    {
+        killRangeText.text = value.ToString();
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeKillRange(bool isPlus)
+    {
+        killRange = (EKillRange)Mathf.Clamp((int)killRange + (isPlus ? 1 : -1), 0, 2);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar (hook = nameof(SetVisualTacks_Hook))]
     private bool visualTasks;
     [SerializeField]
     private Toggle visualTasksToggle;
-    [SyncVar]
+    public void SetVisualTacks_Hook(bool _, bool value)
+    {
+        UpdateGameRuleOverview();
+    }
+    public void OnVisualTasksToggle(bool value)
+    {
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+        visualTasks = value;
+    }
+
+    [SyncVar (hook = nameof(SetTaskBarUpdates_Hook))]
     private ETaskBarUpdates taskBarUpdates;
     [SerializeField]
     private Text taskBarUpdatesText;
-    [SyncVar]
+    public void SetTaskBarUpdates_Hook(ETaskBarUpdates _, ETaskBarUpdates value)
+    {
+        taskBarUpdatesText.text = value.ToString();
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeTaskBarUpdates(bool isPlus)
+    {
+        taskBarUpdates = (ETaskBarUpdates)Mathf.Clamp((int)taskBarUpdates + (isPlus ? 1 : -1), 0, 2);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar (hook = nameof(SetCommonTask_Hook))]
     private int commonTask;
     [SerializeField]
     private Text commonTaskText;
-    [SyncVar]
+    public void SetCommonTask_Hook(int _, int value)
+    {
+        commonTaskText.text = value.ToString();
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeCommonTask(bool isPlus)
+    {
+        commonTask = Mathf.Clamp(commonTask + (isPlus ? 1 : -1), 0, 2);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar (hook = nameof(SetComplexTask_Hook))]
     private int complexTask;
     [SerializeField]
     private Text complexTaskText;
-    [SyncVar]
+    public void SetComplexTask_Hook(int _, int value)
+    {
+        complexTaskText.text = value.ToString();
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeComplexTask(bool isPlus)
+    {
+        complexTask = Mathf.Clamp(complexTask + (isPlus ? 1 : -1), 0, 3);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
+
+    [SyncVar(hook = nameof(SetSimpleTask_Hook))]
     private int simpleTask;
     [SerializeField]
     private Text simpleTaskText;
+    public void SetSimpleTask_Hook(int _, int value)
+    {
+        simpleTaskText.text = value.ToString();
+        UpdateGameRuleOverview();
+    }
+    public void OnChangeSimpleTask(bool isPlus)
+    {
+        simpleTask = Mathf.Clamp(simpleTask + (isPlus ? 1 : -1), 0, 5);
+        isRecommendRule = false;
+        isRecommendRuleToggle.isOn = false;
+    }
 
     [SerializeField]
     private Text gameRuleOverview;
 
     public void UpdateGameRuleOverview()
     {
+        var manager = NetworkManager.singleton as AmongUsRoomManager;
+        StringBuilder sb = new StringBuilder(isRecommendRule ? "추천 설정\n" : "커스텀 설정\n");
+        sb.Append("맵: The Skeld\n");
+        sb.Append($"임포스터: {manager.imposterCount}\n");
+        sb.Append(string.Format("Confirm Ejects: {0}\n", confirmEjects ? "켜짐" : "꺼짐"));
+        sb.Append($"긴급 회의: {emergencyMeetings}\n");
+        sb.Append(string.Format("Anonymous Votes: {0}\n", anonymousVotes ? "켜짐" : "꺼짐"));
+        sb.Append($"긴급 회의 쿨타임: {emergencyMeetingsCooldown}\n");
+        sb.Append($"회의 제한 시간: {meetingsTime}\n");
+        sb.Append($"투표 제한 시간: {voteTime}\n");
+        sb.Append($"이동 속도: {moveSpeed}\n");
+        sb.Append($"크루원 시야: {crewSight}\n");
+        sb.Append($"임포스터 시야: {imposterSight}\n");
+        sb.Append($"킬 쿨타임: {killCooldown}\n");
+        sb.Append($"킬 범위: {killRange}\n");
+        sb.Append($"Tsak Bar Updates: {taskBarUpdates}\n");
+        sb.Append(string.Format("Visual Task: {0}\n", visualTasks ? "켜짐" : "꺼짐"));
+        sb.Append($"공통 임무: {commonTask}\n");
+        sb.Append($"복잡한 임무: {complexTask}\n");
+        sb.Append($"간단한 임무: {simpleTask}\n");
+        gameRuleOverview.text = sb.ToString();
+    }
 
+    private void SetRecommendGameRule()
+    {
+        isRecommendRule = true;
+        confirmEjects = true;
+        emergencyMeetings = 1;
+        emergencyMeetingsCooldown = 15;
+        meetingsTime = 15;
+        voteTime = 120;
+        moveSpeed = 1f;
+        crewSight = 1f;
+        imposterSight = 1.5f;
+        killCooldown = 45f;
+        killRange = EKillRange.Normal;
+        visualTasks = true;
+        commonTask = 1;
+        complexTask = 1;
+        simpleTask = 2;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (isServer)
+        {
+            SetRecommendGameRule();
+        }
     }
 
     // Update is called once per frame
--- AmongUs/Assets/UI/GameRoom/Scripts/CustomaizeUI.cs
+++ AmongUs/Assets/UI/GameRoom/Scripts/CustomaizeUI.cs
@@ -6,6 +6,11 @@ using Mirror;
 
 public class CustomaizeUI : MonoBehaviour
 {
+    [SerializeField] private Button colorButton;
+    [SerializeField] GameObject colorPanel;
+    [SerializeField] Button gameRuleButton;
+    [SerializeField] GameObject gameRulePanel;
+
     [SerializeField] private Image characterPreview;
     [SerializeField] private List<ColorSelectButton> colorSelectButtons;
 
@@ -16,9 +21,28 @@ public class CustomaizeUI : MonoBehaviour
         characterPreview.material = inst;
     }
 
+    public void ActiveColorPanel()
+    {
+        colorButton.image.color = new Color(0f, 0f, 0f, 0.75f);
+        gameRuleButton.image.color = new Color(0f, 0f, 0f, 0.25f);
+
+        colorPanel.SetActive(true);
+        gameRulePanel.SetActive(false);
+    }
+
+    public void ActiveGameRulePanel()
+    {
+        colorButton.image.color = new Color(0f, 0f, 0f, 0.25f);
+        gameRuleButton.image.color = new Color(0f, 0f, 0f, 0.75f);
+
+        colorPanel.SetActive(false);
+        gameRulePanel.SetActive(true);
+    }
+
     private void OnEnable()
     {
         UpdateColorButton();
+        ActiveColorPanel();
 
         var roomSlots = (NetworkManager.singleton as AmongUsRoomManager).roomSlots;
         foreach (var player in roomSlots)
